b5458f28cf5164730688a304d5e7a08c637277d7
Jose Antonio Parra <antoniojose558@gmail.com>
2018-05-24 16:22:57 -0400

    Servicio de Firebase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -6,14 +6,31 @@ export class InformacionService {
 
 info: any = {};
 cargada = false;
+cargada_sobre_nosotros = false;
+equipo: any[] = [];
 
   constructor(public http: Http) {
 
+  this.carga_info();
+  this.carga_sobre_nosotros();
+  }
+
+  public carga_info() {
     this.http.get('assets/data/info.pagina.json')
+    .subscribe(data => {
+      // console.log(data.json());
+      this.cargada = true;
+      this.info = data.json();
+    });
+
+  }
+
+  public carga_sobre_nosotros() {
+    this.http.get('https://paginawebtpa.firebaseio.com/equipo.json')
       .subscribe(data => {
         console.log(data.json());
         this.cargada = true;
-        this.info = data.json();
+        this.equipo = data.json();
       });
 
   }
