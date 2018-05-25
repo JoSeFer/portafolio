b3e6c928f5a6d3bfa099b28cdb3612a20c51ebcc
Jose Antonio Parra <antoniojose558@gmail.com>
2018-05-25 15:59:24 -0400

    Agregamos imagenes y servicio de Productos

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -28,7 +28,7 @@ equipo: any[] = [];
   public carga_sobre_nosotros() {
     this.http.get('https://paginawebtpa.firebaseio.com/equipo.json')
       .subscribe(data => {
-        console.log(data.json());
+        // console.log(data.json());
         this.cargada = true;
         this.equipo = data.json();
       });
---
+++ src/app/services/productos.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+
+@Injectable()
+export class ProductosService {
+productos: any[] = [];
+cargando_productos = true;
+  constructor(private http: Http) {
+    this.cargar_productos();
+   }
+
+  public cargar_productos() {
+
+      this.http.get('https://paginawebtpa.firebaseio.com/productos_idx.json').subscribe( res => {
+        console.log(res.json());
+        this.cargando_productos = false;
+      });
+  }
+
+}
